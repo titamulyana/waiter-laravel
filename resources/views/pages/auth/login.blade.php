8d93d95f37ecdf2164b55e8e394b4d26f7a648c4
titamulyana <tita@ilcs.co.id>
2023-03-22 23:11:49 +0700

    update login page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -7,13 +7,13 @@
         <div class="text-center mb-11">
             <!--begin::Title-->
             <h1 class="text-dark fw-bolder mb-3">
-                Sign In
+                Bale Ayam Nusantara
             </h1>
             <!--end::Title-->
 
             <!--begin::Subtitle-->
             <div class="text-gray-500 fw-semibold fs-6">
-                Your Social Campaigns
+                Food Court
             </div>
             <!--end::Subtitle--->
         </div>
@@ -21,38 +21,7 @@
 
         <!--begin::Login options-->
         <div class="row g-3 mb-9">
-            <!--begin::Col-->
-            <div class="col-md-6">
-                <!--begin::Google link--->
-                <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                    <img alt="Logo" src="{{ image('svg/brand-logos/google-icon.svg') }}" class="h-15px me-3"/>
-                    Sign in with Google
-                </a>
-                <!--end::Google link--->
-            </div>
-            <!--end::Col-->
-
-            <!--begin::Col-->
-            <div class="col-md-6">
-                <!--begin::Google link--->
-                <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                    <img alt="Logo" src="{{ image('svg/brand-logos/apple-black.svg') }}" class="theme-light-show h-15px me-3"/>
-                    <img alt="Logo" src="{{ image('svg/brand-logos/apple-black-dark.svg') }}" class="theme-dark-show h-15px me-3"/>
-                    Sign in with Apple
-                </a>
-                <!--end::Google link--->
-            </div>
-            <!--end::Col-->
         </div>
-        <!--end::Login options-->
-
-        <!--begin::Separator-->
-        <div class="separator separator-content my-14">
-            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-        </div>
-        <!--end::Separator-->
-
-        <!--begin::Input group--->
         <div class="fv-row mb-8">
             <!--begin::Email-->
             <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" value="demo@demo.com"/>
@@ -82,20 +51,10 @@
         <!--begin::Submit button-->
         <div class="d-grid mb-10">
             <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                @include('partials/general/_button-indicator', ['label' => 'Sign In'])
+                @include('partials/general/_button-indicator', ['label' => 'Login'])
             </button>
         </div>
         <!--end::Submit button-->
-
-        <!--begin::Sign up-->
-        <div class="text-gray-500 text-center fw-semibold fs-6">
-            Not a Member yet?
-
-            <a href="/register" class="link-primary">
-                Sign up
-            </a>
-        </div>
-        <!--end::Sign up-->
     </form>
     <!--end::Form-->
 
